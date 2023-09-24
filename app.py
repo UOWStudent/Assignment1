142b5ce2759dcb1e7d8053ae86ac3a5ada330645
UOWStudent <140778909+UOWStudent@users.noreply.github.com>
2023-09-24 22:09:31 +1000

    Create app.py

---
+++ app.py
@@ -0,0 +1,146 @@
+import re
+import html
+import urllib.parse
+from flask import Flask, render_template
+import myscript  # Import Python script
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def validate_user_input(input_string):
+  """Validates user input for malicious code or characters.
+
+  Args:
+    input_string: The user input to be validated.
+
+  Returns:
+    A boolean value indicating whether the input is valid.
+  """
+
+  # Check for malicious code or characters.
+  if re.search(r'<|script|iframe|alert', input_string):
+    return False
+
+  # Check for banned characters.
+  banned_characters = ['"', "'", '<', '>', '&']
+  for character in banned_characters:
+    if character in input_string:
+      return False
+
+  return True
+
+def sanitize_data(input_string):
+  """Sanitizes user input by escaping special characters, including HTML.
+
+  Args:
+    input_string: The user input to be sanitized.
+
+  Returns:
+    A sanitized string.
+  """
+
+  return html.escape(input_string)
+
+def encode_client_output(output_string):
+  """Encodes client output to prevent XSS attacks.
+
+  Args:
+    output_string: The client output to be encoded.
+
+  Returns:
+    An encoded string.
+  """
+
+  return output_string.encode('utf-8')
+
+def encode_server_output(output_string):
+  """Encodes server output to prevent XSS attacks.
+
+  Args:
+    output_string: The server output to be encoded.
+
+  Returns:
+    An encoded string.
+  """
+
+  return output_string.encode('utf-8')
+
+def enable_http_only_flag(cookie):
+  """Enables the HttpOnly flag on a cookie.
+
+  Args:
+    cookie: The cookie to enable the HttpOnly flag on.
+  """
+
+  cookie['httponly'] = True
+
+def escape_html_characters(input_string):
+  """Escapes HTML characters in a string.
+
+  Args:
+    input_string: The string to escape HTML characters in.
+
+  Returns:
+    A string with HTML characters escaped.
+  """
+
+  return html.escape(input_string)
+
+def use_safe_dom_function(dom_function):
+  """Wraps a DOM function to prevent XSS attacks.
+
+  Args:
+    dom_function: The DOM function to wrap.
+
+  Returns:
+    A wrapped DOM function.
+  """
+
+  def safe_dom_function(*args, **kwargs):
+    for arg in args:
+      if not isinstance(arg, str):
+        continue
+
+      # Escape HTML characters in the argument.
+      arg = escape_html_characters(arg)
+
+    return dom_function(*args, **kwargs)
+
+  return safe_dom_function
+
+def enforce_csp(csp_policy):
+  """Enforces a content security policy.
+
+  Args:
+    csp_policy: The content security policy to enforce.
+  """
+
+  # Set the Content-Security-Policy header.
+  response.headers['Content-Security-Policy'] = csp_policy
+
+def secure_get_request(request):
+  """Secures a GET request by validating all user input and escaping HTML characters.
+
+  Args:
+    request: The GET request to secure.
+
+  Returns:
+    A secured GET request.
+  """
+
+  # Validate all user input in the query parameters.
+  for parameter, value in request.query_params.items():
+    if not validate_user_input(value):
+      # The user input is invalid.
+      raise ValueError('Invalid GET parameter: {}'.format(parameter))
+
+  # Escape HTML characters in the query parameters.
+  for parameter, value in request.query_params.items():
+    request.query_params[parameter] = escape_html_characters(value)
+
+  return request
+
+def secure_post_request(request):
+  """Secures a POST request by validating all user input and escaping HTML characters
+"""
